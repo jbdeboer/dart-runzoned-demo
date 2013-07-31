314b5b2297d57955436f5d08c341e7ce6ee0b5ae
James deBoer <james@huronbox.com>
2013-07-31 15:26:29 -0700

    runZoned example

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,26 @@
 import 'dart:html' as dom;
 import 'dart:math' as math;
+import 'dart:async';
 
 import 'package:angular/angular.dart';
 
+// THIS code prints:
+// running
+// yahoo: 357457
+// done
+
 main() {
-  var module = new AngularModule();
-  bootstrapAngular([module]);
+  runZonedExperimental(() {
+    Completer c = new Completer();
+    var future = c.future;
+    var httpFuture = dom.HttpRequest.getString("http://yahoo.com");
+
+    httpFuture.then((data) => c.complete(data));
+
+    future.then((data) { print("yahoo: ${data.length}");});
+  }, onRunAsync: (fn) {
+    print("running");
+    fn();
+    print("done");
+  });
 }
